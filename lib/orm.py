b7224cbeaf43223b980d979939f90f78788762a2
z <z>
2016-01-10 10:51:43 +1100

    Models

--- lib/orm.py
+++ lib/orm.py
@@ -1,3 +1,13 @@
 class ORM:
+  def __init__(self):
+    pass
+  
   def save(self):
-    
+    table_name = self.__class__.__name__.lower()+"s"
+	cur = conn.cursor()
+  
+  @staticmethod
+  def find(id):
+	table_name = self.__class__.__name__.lower()+"s"
+    cur = conn.cursor()
+    cursor.execute('''SELECT * FROM ? WHERE id=?''',(table_name,id))
