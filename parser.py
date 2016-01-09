c9c94bb2db91da033768ff3f888de4f67982706d
leaen <mossbanay@gmail.com>
2016-01-09 17:41:06 +1100

    Added support for IfNode and ForNode

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from nodes import GroupNode, TextNode, PythonNode
+from nodes import GroupNode, TextNode, PythonNode, ForNode, IfNode
 
 class Parser(object):
     def __init__(self, context, tokenzier=None):
@@ -13,9 +13,48 @@ class Parser(object):
         return TextNode(token)
 
     def _parse_tag_stmt(self, token):
-        raise NotImplementedError()
+        op, args = self._remove_tag(token).split(maxsplit=1)
 
-    def _parse_for(self):
+        if op == 'for':
+            node = self._parse_for(token)
+        elif op == 'if':
+            node = self._parse_if(token)
+
+        return node
+
+    def _parse_if(self, token):
+        op, args = self._remove_tag(token).split(maxsplit=1)
+        predicate = ' '.join(args)
+
+        if_node = IfNode(predicate)
+
+        group = GroupNode()
+
+        for token, token_type in self._tokenizer:
+            if token_type == 'literal':
+                node = self._parse_literal(token)
+            elif token_type == 'tag_stmt':
+                # Check if we are looking at an {% end if %} tag
+                op, args = self._remove_tag(token).split(maxsplit=1)
+                if op == 'end' and args[0] == 'if':
+                    break
+                else:
+                    node = self._parse_tag_stmt(token)
+            elif token_type == 'tag_repr':
+                node = self._parse_tag_repr(token)
+
+            group.add_child(node)
+
+        if_node.add_child(group)
+
+        return if_node
+
+    def _parse_for(self, token):
+        op, args = self._remove_tag(token).split(maxsplit=1)
+        item, collection = args
+
+        for_node = ForNode(item, collection)
+
         group = GroupNode()
 
         for token, token_type in self._tokenizer:
@@ -33,7 +72,9 @@ class Parser(object):
 
             group.add_child(node)
 
-        return group
+        for_node.add_child(group)
+
+        return for_node
 
     def _parse_tag_repr(self, token):
         node = PythonNode()
