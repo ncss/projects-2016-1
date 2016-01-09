1185d5c90dcf5b8196d336e30f17996e88ed1fed
Kat956 <kathryngill97@gmail.com>
2016-01-09 17:42:12 +1100

    Initial Server Tests
    Added some stuff. Not really important.

--- server.py
+++ server.py
@@ -2,8 +2,15 @@ from tornado.ncss import Server
 
 server = Server()
 
+def index_handler(response):
+    response.write("<h1>Hello, World! </h1>")
+
+
+def meme_handler(response):
+    response.write("m" + "e"*1337 + "mes")
 # Handlers
-server.register(r'/',index_handler)
+server.register(r'/', index_handler)
+server.register(r'/memes', meme_handler)
 
 
 server.run()
