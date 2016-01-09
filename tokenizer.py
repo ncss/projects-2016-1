8fb4f038ff780439bcc96ddc93fbb7d61b708be7
Sam Leatherdale <spammyl@hotmail.com>
2016-01-09 16:00:40 +1100

    Updated tokenizer, it now works
    :)

--- tokenizer.py
+++ tokenizer.py
@@ -1,19 +1,25 @@
 def tokenize(filename):
     text = open(filename).read()
     tokens = []
-    in_token = False
+    in_special_token = False
+    literal_token = []
     symbol_start = -1
     for i in range(len(text)-1):
         symbol = text[i:i+2]         
         if (symbol == "{%" or symbol == "{{"):
+            if len(literal_token) > 0:
+                tokens.append("".join(literal_token[1:]))
+                literal_token = []
             symbol_start = i
-            in_token = True
+            in_special_token = True
         elif (symbol == "%}" or symbol == "}}"):
             tokens.append(text[symbol_start:i+2])
-            in_token = False
+            in_special_token = False
+        elif in_special_token == False:
+            literal_token.append(text[i])
     return tokens
 
-#print(tokenize("test_tokenizer.txt"))
+print(tokenize("test_tokenizer.txt"))
 
         
         
