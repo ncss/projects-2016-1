226d891f1bb087670e6c0256cccae98c2082aed8
z <z>
2016-01-10 11:53:01 +1100

    More models

---
+++ sql/populate.py
@@ -0,0 +1,11 @@
+import sqlite3
+import hashlib
+
+conn = sqlite3.connect("database.db")
+
+cur = conn.cursor()
+for i in range(1000):
+  cur.execute('''INSERT INTO users (username,password)
+                VALUES (?,?);''',("User"+str(i),hashlib.sha512(str("pw"+str(i)).encode('utf-8')).hexdigest()));
+
+conn.commit()
