68fbfad5d0922fb8f465bff7e96eec0790096ef2
Liv Gorton <olivia.k.gorton@gmail.com>
2016-01-10 14:59:53 +1100

    Created Handlers File
    - moved handler functions to handlers.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from tornado.ncss import Server
 
-import server.util as util
+import server.handlers as handlers
 
 '''
 TO DO LIST:
@@ -14,59 +14,18 @@ TO DO LIST:
     - settings_handler
 '''
 
-
 server = Server()
 
-@util.requires_login
-def index_handler(response):
-    response.write("<h1>Hello, World! </h1> <p>user_id = {}</p>".format(response.get_secure_cookie('user_id')))
-    #if cookie[user_id] != None:
-        #username = database.get_username(cookie["user_id"])
-        #response.write(render_template("index.html", username = username))
-    #else:
-        #response.write(render_template("index.html", username = None)                                  
-
-def login_handler(response):
-    response.set_secure_cookie('user_id', '-1')
-    response.write("<h1>Login Page</h1>")
-    response.redirect('/')
-
-# messing around with login handler clearing cookie and redirecting to a page
-def logout_handler(response):
-    response.clear_cookie('user_id')
-    response.redirect('/')
-
-def feed_handler(response):
-    response.write("<h1> ( ͡° ͜ʖ ͡°) FEED ( ͡° ͜ʖ ͡°) </h1>")
-
-# dashboard integrates profile
-def dashboard_handler(response):
-    response.write("<h1> ( ͡° ͜ʖ ͡°) DASHBOARD ( ͡° ͜ʖ ͡°) </h1>")
-
-def create_handler(response):
-    response.write("<h1> ( ͡° ͜ʖ ͡°) CREATE DEM MISTS ( ͡° ͜ʖ ͡°) </h1>")
-
-# NEED MIST ID BEFORE THIS WILL WORK
-#def view_handler(reponse):
-    #response.write("<h1> ( ͡° ͜ʖ ͡°) VIEW DEM MISTS ( ͡° ͜ʖ ͡°) </h1>")
-    
-# NEED MIST ID BEFORE THIS WILL WORK
-#def edit_handler(response):
-    #response.write("<h1> ( ͡° ͜ʖ ͡°) EDIT DEM MISTS ( ͡° ͜ʖ ͡°) </h1>")
-
-def settings_handler(response):
-    response.write("<h1> ( ͡° ͜ʖ ͡°) CHANGE YA PROFILE SETTINGS ( ͡° ͜ʖ ͡°) </h1>")
-                     
 
 # Handlers
-server.register(r'/', index_handler)
-server.register(r'/login', login_handler)
-server.register(r'/logout', logout_handler)
-server.register(r'/feed', feed_handler)
-server.register(r'/dashboard', dashboard_handler)
-server.register(r'/create', create_handler)
-#server.register(r'/{}/view'.format(), view_handler)
-#server.register(r'/{}/edit'.format(), edit_handler)
-server.register(r'/settings', settings_handler)
+server.register(r'/', handlers.index_handler)
+server.register(r'/login', handlers.login_handler)
+server.register(r'/logout', handlers.logout_handler)
+server.register(r'/feed', handlers.feed_handler)
+server.register(r'/dashboard', handlers.dashboard_handler)
+server.register(r'/create', handlers.create_handler)
+#server.register(r'/{}/view'.format(), handlers.view_handler)
+#server.register(r'/{}/edit'.format(), handlers.edit_handler)
+server.register(r'/settings', handlers.settings_handler)
 
 server.run()
