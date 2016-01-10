90c2d01bda34a9c787ae4557d93b623e03637e14
Alexander Hogue <defaultnamehere@gmail.com>
2016-01-10 17:22:32 +1100

    A fix to the templater example script

--- templater_example.py
+++ templater_example.py
@@ -8,4 +8,6 @@ user = User("Tim Dawborn")
 print(templater.render('docs/templater.md', user=user,
                        movie_list=["Aladdin", "Finding Nemo"],
                        var_name="Example variable",
+                       page_title="Movie list",
+                       site_title="List thing site",
                        expression=True))
