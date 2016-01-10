dfd9a411fe82e054dbe0215b9eaa1a50402e3849
Tom <lecafard@users.noreply.github.com>
2016-01-10 15:38:41 +1100

    Fix from_row

--- models/list.py
+++ models/list.py
@@ -1,24 +1,42 @@
+def convert_row(row):
+  keys = row.keys()
+  return {key:row[idx] for idx,key in enumerate(keys)}
+
 class List:
-  # Definitely needed to connect to database
-  @staticmethod
-  def connect(db):
-    global conn
-    conn = db
-  
+  @classmethod
+  def connect(cls,db):
+    cls.conn = db
+
   def __init__(self, list, item_order):
     self.list = list
     self.item_order = item_order
 
-  def find(self, id):
-    cur = conn.cursor()
-    cur.execute('select * FROM lists WHERE id=?', (id,))
+  @classmethod
+  def find(cls, id):
+    cur = cls.conn.cursor()
+    cur.execute('SELECT * FROM lists WHERE id=?', (id,))
+
+  @classmethod
+  def find_by_userid(cls, uid):
+    cur = cls.conn.cursor()
+    cur.execute('SELECT * FROM list where author=?', (uid,))
+    results = cur.fetchall()
+    cur.close()
+    return results
+
+  @classmethod
+  def from_row(cls,row):
+    if row is None: return None
+    row = convert_row(result)
+    return cls(saved=True,**row)
+
 
   '''def  (self, ):
     cur = conn.cursor()
     con.execute(
-  
 
-  
+
+
   @staticmethod
   def find(self, limit):
     pass
