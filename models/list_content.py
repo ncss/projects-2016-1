d2096209eccac9cfe7382c89dbe1af557900986d
deannafabrello <deanna.fabrello@sydstu.catholic.edu.au>
2016-01-10 15:04:33 +1100

    updated search in list_content

--- models/list_content.py
+++ models/list_content.py
@@ -19,21 +19,42 @@ class ListContent:
       raise UserNotFound('{} does not exist'.format(content))
     return ListContent(row[0], row[1], row[2])
 
+  @staticmethod
   def findByListId(list_id):
     cur = conn.execute('''SELECT * FROM list_contents WHERE list=?''', (list_id,))
     rows = cur.fetchall()
-    for row in rows:
-      print(row)
-      #item= ListContent
       
-    return []
+    list = []
+    for row in rows:
+      item = ListContent(row[1],row[2],row[3])
+      list.append(item)
 
+    return list
+  
+  @staticmethod
+  def search(keyword):
+    
+    fuzzy_matcher = '%' + keyword + '%'
+    cur = conn.execute("""SELECT * FROM list_contents WHERE content LIKE ? """, ( fuzzy_matcher,))
+    rows = cur.fetchall()
+
+    list = []
+    for row in rows:
+      item = ListContent(row[1],row[2],row[3])
+      list.append(item)
+
+    return list
+  
 if __name__ == '__main__':
   conn = sqlite3.connect(':memory:')
   conn.executescript(open('../sql/init.sql').read())
   print("Welcome to the ListContent module.")
   # create a new content item
   c = ListContent("test item 1",0,5)
-  ListContent.findByListId(0)
-  
+  print(ListContent.findByListId(0))
+  results= ListContent.search('movie')
+  print(results)
 
+  for r in results:
+    print("Content: ", r.content)
+ 
