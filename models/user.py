7823d84a6eac97ece0ef3d5ab8fdafcb759aef21
z <lecafard@users.noreply.github.com>
2016-01-10 12:49:39 +1100

    Add rows, auth working

--- models/user.py
+++ models/user.py
@@ -1,33 +1,64 @@
+
+import sqlite3
+conn = sqlite3.connect("database.db")
+conn.row_factory = sqlite3.Row
+
 import hashlib
 
+def convert_row(row):
+  keys = row.keys()
+
+  return {key:row[idx] for idx,key in enumerate(keys)}
+
+
 class User:
-  def __init__(self,username,password,email):
-    self.username = username
-    self.password = password
-    self.email = email
-    self._saved = saved
+  def __init__(self,saved=False,**kwargs):
+    self.username = kwargs["username"]
+    self.password = kwargs["password"]
+    self.saved = saved
+    
 
   def check_password(self,password):
-    return self.password == hashlib.sha512(password).hexdigest()
+    return self.password == hashlib.sha512(str(password).encode('utf-8')).hexdigest()
 	
-  def new_list(self):
+  def get_lists(self):
     pass
 
+  def save(self):
+    cur = con.cursor()
+    if not self._saved:
+      cur.execute('INSERT INTO users (username,password,email)VALUES=(?,?,?);', (self.username,self.password,self.email));
+    else:
+      cur.execute('''
+        UPDATE users
+        SET username=?,
+            password=?,
+            email=?
+      ''',(self.username,self.password,self.email));
+    conn.commit()
+    cur.close()
+  
   @staticmethod
-  def find(username):
+  def findUsername(username):
     cur = conn.cursor()
-
+    cur.execute('SELECT * FROM users WHERE username=?', (self.username))
+    return User(cur.fetchone())
+    cur.close()
+  
   @staticmethod
   def new_user(self, *args):
     pass
 
   @staticmethod
   def authenticate(username,password):
-    hashed_pw = hashlib.sha512(password).hexdigest()
+    hashed_pw = hashlib.sha512(str(password).encode('utf-8')).hexdigest()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM users WHERE username=? AND password=?',(username,hashed_pw))
+    cur.execute('SELECT * FROM users WHERE username=? AND password=?;', (username,hashed_pw))
 
     result = cur.fetchone()
+    cur.close() 
     if result is None: return False
-    return User(**result)
+    result = convert_row(result)
+    return User(saved=True,**result)
+
 
