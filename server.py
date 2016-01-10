802b49347907164c86c3ae0742af89ac27c0a9d8
Liv Gorton <olivia.k.gorton@gmail.com>
2016-01-10 11:54:00 +1100

    Adding more handlers and registering URLS

--- server.py
+++ server.py
@@ -4,12 +4,28 @@ print(sys.version_info)
 
 from tornado.ncss import Server
 
+'''
+TO DO LIST:
+- add list_id to the URL for registering view and edit
+- complete handler functions for:
+    - feed_handler
+    - dashboard_handler
+    - create_handler
+    - view_handler
+    - edit_handler
+    - settings_handler
+'''
+
+
 server = Server()
 
 def index_handler(response):
     response.write("<h1>Hello, World! </h1> <p>user_id = {}</p>".format(response.get_secure_cookie('user_id')))
-    
-
+    #if cookie[user_id] != None:
+        #username = database.get_username(cookie["user_id"])
+        #response.write(render_template("index.html", username = username))
+    #else:
+        #response.write(render_template("index.html", username = None)                                  
 
 def login_handler(response):
     response.set_secure_cookie('user_id', '-1')
@@ -20,10 +36,36 @@ def login_handler(response):
 def logout_handler(response):
     response.clear_cookie('user_id')
     response.redirect('/')
-    
+
+def feed_handler(response):
+    ...
+
+# dashboard integrates profile
+def dashboard_handler(response):
+    ...
+
+def create_handler(response):
+    ...
+
+def view_handler(reponse):
+    ...
+
+def edit_handler(response):
+    ...
+
+def settings_handler(response):
+    ...
+                     
+
 # Handlers
 server.register(r'/', index_handler)
 server.register(r'/login', login_handler)
 server.register(r'/logout', logout_handler)
+server.register(r'/feed', feed_handler)
+server.register(r'/dashboard', dashboard_handler)
+server.register(r'/create', create_handler)
+#server.register(r'/{}/view'.format(), view_handler)
+#server.register(r'/{}/edit'.format(), edit_handler)
+server.register(r'/dashboard/settings', dashboard_handler)
 
 server.run()
