f9f396fa2f972693561f746497c0fb7a260ce69f
deannafabrello <deanna.fabrello@sydstu.catholic.edu.au>
2016-01-10 12:42:37 +1100

    created findbylist id method

--- models/list_content.py
+++ models/list_content.py
@@ -1,3 +1,7 @@
+import sqlite3
+
+
+
 class ListContent:
   def __init__(self, content, list_num, item_order):
     self.content = content
@@ -13,10 +17,21 @@ class ListContent:
       raise UserNotFound('{} does not exist'.format(content))
     return ListContent(row[0], row[1], row[2])
 
+  def findByListId(list_id):
+    cur = conn.execute('''SELECT * FROM list_contents WHERE list=?''', (list_id,))
+    rows = cur.fetchall()
+    for row in rows:
+      print(row)
+      #item= ListContent
+      
+    return []
 
 if __name__ == '__main__':
+  conn = sqlite3.connect(':memory:')
+  conn.executescript(open('../sql/init.sql').read())
   print("Welcome to the ListContent module.")
   # create a new content item
-  c = ListContent("test item 1")
+  c = ListContent("test item 1",0,5)
+  ListContent.findByListId(0)
   
 
