3253411311a07d192df3abc693cd4a9c3ee5788a
Liv Gorton <olivia.k.gorton@gmail.com>
2016-01-10 11:09:38 +1100

    Clearing Cookies at Logout
    On /logout, cookies get cleared and user is redirected to "/".
    
    ( ͡° ͜ʖ ͡°)

--- server.py
+++ server.py
@@ -11,11 +11,15 @@ def login_handler(response):
     response.set_secure_cookie('user_id', '-1')
     response.write("<h1>Login Page</h1>")
     response.redirect('/')
-  
+
+# messing around with login handler clearing cookie and redirecting to a page
+def logout_handler(response):
+    response.clear_cookie('user_id', '-1')
+    response.redirect('/')
     
 # Handlers
 server.register(r'/', index_handler)
 server.register(r'/login', login_handler)
-
+server.register(r'/logout', logout_handler)
 
 server.run()
