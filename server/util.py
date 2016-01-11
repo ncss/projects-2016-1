2a19c7cd63ff41ebe6d1f7616398fbe4dba48ded
Jackson Gatenby <jackson.gatenby@gmail.com>
2016-01-12 04:56:56 +1100

    Move some functions from handlers to server util

--- server/util.py
+++ server/util.py
@@ -9,3 +9,12 @@ def requires_login(fn):
         else:
             response.write(templater.render("templates/login_page.html", page_title="Login", site_title="M'lists",login_failed=False))
     return result
+
+def get_current_user_id(response):
+    uid = response.get_secure_cookie("user_id")
+    if uid is None:
+        raise Exception("No user is currently logged in")
+    return uid
+
+def is_logged_in(response):
+    return response.get_secure_cookie("user_id") is not None
