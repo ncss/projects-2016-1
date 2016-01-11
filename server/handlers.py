a34c939554b875af7c2dcf32e96456028a67f946
leaen <mossbanay@gmail.com>
2016-01-12 04:13:47 +1100

    Fixed merge issue from before

--- server/handlers.py
+++ server/handlers.py
@@ -166,13 +166,22 @@ def post_like_handler(response):
     l = Likes(user_id, list_id)
     l.create(user_id, list_id)
 
-    likes = 100
+    likes = Likes.list_likes(list_id)
 
-    response.write(json.dumps({'likes':likes}))
     response.set_header('Content-Type', 'application/json')
-	
+    response.write(json.dumps({'likes':likes}))
+
 def post_unlike_handler(response):
-	response.write("<h1> ( ͡° ͜ʖ ͡°) UNLIKE YA LIST ( ͡° ͜ʖ ͡°) </h1>")
+    user_id = response.get_field('user_id')
+    list_id = response.get_field('list_id')
+
+    l = Likes(user_id, list_id)
+    l.remove(user_id, list_id)
+
+    likes = Likes.list_likes(list_id)
+
+    response.set_header('Content-Type', 'application/json')
+    response.write(json.dumps({'likes':likes}))
 
 def get_current_user_id(response):
     uid = response.get_secure_cookie("user_id")
@@ -187,6 +196,7 @@ def is_logged_in(response):
 
 def page_not_found_handler(response, path):
     #insert a html page for 404
+    response.set_status(404, 'Page not found')
     response.write(templater.render("templates/404.html", page_title="Page not found", site_title="M'lists"))
 
 
