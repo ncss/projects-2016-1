936056dfccf1f763522aaa839cc4d4ab16b5ca1f
leaen <mossbanay@gmail.com>
2016-01-11 15:52:20 +1100

    Cleaned likes.py

--- models/likes.py
+++ models/likes.py
@@ -1,13 +1,9 @@
 class Likes:
-  # Definitely needed to connect to database
-  @classmethod
-  def connect(cls, db):
-    cls.conn = db
 
-  def __init__(self,like_user, like_listnum, like_id):
-    self.like_user = like_user
-    self.like_listnum = like_listnum
-    self.like_id = like_id
-	
+    @classmethod
+    def connect(cls, db):
+        cls.conn = db
 
-  
+    def __init__(self, user_id, list_id):
+      self.user_id = user_id
+      self.list_id = list_id
